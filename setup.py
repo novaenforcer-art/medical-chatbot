942f4ec8072db04d45bda2a107ff2ef6ba7bbe1b
novaenforcer-art <82377474+novaenforcer-art@users.noreply.github.com>
2025-05-13 17:10:43 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Geneerative AI Project',
+    version = '0.0.0',
+    author = 'Naman',
+    author_email='sahunama6302@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
